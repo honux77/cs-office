4389d8e06ba42ac3a46e71b75cf99304dcebe3bd
Hoyoung Jung <honux77@gmail.com>
2025-02-15 13:01:36 +0900

    셀프 리뷰 반영 저장소 테스트 코드 추가

--- src/main/java/kr/codesquad/office/Book.java
+++ src/main/java/kr/codesquad/office/Book.java
@@ -1,16 +1,14 @@
 package kr.codesquad.office;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
-@Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class Book {
 
     @Id
